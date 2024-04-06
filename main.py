2108dc9aa44cc36ce023579aacec8e0f14b017f6
joe-newm <j.newm500@gmail.com>
2024-04-05 21:23:05 -0500

    more setup code

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ lower_margin = 100
 side_margin = 300
 
 screen = pygame.display.set_mode((screen_width + side_margin, screen_height + lower_margin))
+pygame.display.set_caption("Level Editor")
+
+#define game variables
+scroll_left = False
+scroll_right = False
+scroll = 0
+scroll_speed = 1
 
 run = True
 while run:
